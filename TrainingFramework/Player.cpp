5e7ac31d9eff4aa3f2e3680d37cc919c30e456ca
Tien Nguyen <tien.nd184311@sis.hust.edu.vn>
2021-08-13 15:45:29 +0800

    + add attribute gold, hp

--- TrainingFramework/Player.cpp
+++ TrainingFramework/Player.cpp
@@ -9,10 +9,19 @@ Player::Player() {}
 Player::Player(std::string prefabID, Vector2 roomID, Matrix translationMatrix)
 	: Character(prefabID, roomID, translationMatrix) {
 	m_strState = IDLE_LEFT;
+
+	m_inumGold = 0;
+	std::string goldText = "Gold: " + std::to_string(m_inumGold);
+	numGoldText = new Text(goldText, 1, 1, TEXT_COLOR::GREEN, 20, 40, 1.0f);
+
+	m_iCurHP = 100;
+	std::string Hptext = "HP: " + std::to_string(m_iCurHP);
+	numHPText = new Text(Hptext, 1, 1, TEXT_COLOR::RED, 20, 60, 1.0f);
 }
 
 Player::~Player() {
-
+	delete numGoldText;
+	delete numHPText;
 }
 
 void Player::Update(float frameTime) {
@@ -23,4 +32,19 @@ void Player::Update(float frameTime) {
 
 void Player::Render(Camera* camera) {
 	Renderer::GetInstance()->DrawAnimated(this, camera);
+	Renderer::GetInstance()->DrawText2(numGoldText);
+	Renderer::GetInstance()->DrawText2(numHPText);
+}
+
+
+void Player::increaseGold(Gold* gold) {
+	m_inumGold += gold->getValue();
+}
+
+void Player::increaseGold(int value) {
+	m_inumGold += value;
+}
+
+void Player::increaseHP(int numHP) {
+	m_iCurHP += numHP;
 }
--- TrainingFramework/Player.h
+++ TrainingFramework/Player.h
@@ -1,8 +1,16 @@
 #pragma once
 #include "Character.h"
+#include "Text.h"
+#include"Gold.h"
 
 class Player : public Character {
+private:
+	
 public:
+	Text* numGoldText;
+	int m_inumGold;
+	Text* numHPText;
+	int m_iCurHP;
 
 
 	Player();
@@ -11,4 +19,9 @@ public:
 
 	void Update(float frameTime);
 	void Render(Camera *camera);
+
+	//tien add
+	void increaseGold(Gold* gold);
+	void increaseGold(int value);
+	void increaseHP(int numHP);
 };
