d7a1e4b4f87400dd7bf59cfc3b121c4e3607b0b8
Tien Nguyen <tien.nd184311@sis.hust.edu.vn>
2021-08-13 15:44:16 +0800

    + add some attribute

--- TrainingFramework/Enemy.cpp
+++ TrainingFramework/Enemy.cpp
@@ -9,10 +9,12 @@ Enemy::Enemy() {}
 Enemy::Enemy(std::string prefabID, Vector2 roomID, Matrix translationMatrix)
 	: Character(prefabID, roomID, translationMatrix) {
 	m_strState = IDLE_LEFT;
+	m_translationMatrix = translationMatrix;
+	isDead = false;
 }
 
 Enemy::~Enemy() {
-
+	//delete m_gold;
 }
 
 void Enemy::Update(float frameTime) {
@@ -21,4 +23,10 @@ void Enemy::Update(float frameTime) {
 
 void Enemy::Render(Camera* camera) {
 	Renderer::GetInstance()->DrawAnimated(this, camera);
+}
+
+void Enemy::createGoldObject() {
+	Matrix translation = getTranslationMatrix();
+	translation.SetTranslation(translation.m[3][0] + 2, translation.m[3][1] + 2, translation.m[3][2]);
+	m_gold = new Gold(GOLD, m_RoomID, translation, rand() % 100 + 1, false);
 }
--- TrainingFramework/Enemy.h
+++ TrainingFramework/Enemy.h
@@ -1,9 +1,15 @@
 #pragma once
 #include "Character.h"
+#include"Gold.h"
 
 class Enemy : public Character {
+private:
+	Gold* m_gold;
+	int m_inumGold;	
+	Matrix m_translationMatrix;
+	
 public:
-
+	bool isDead ;
 
 	Enemy();
 	Enemy(std::string prefabID, Vector2 roomID, Matrix translationMatrix);
@@ -11,4 +17,9 @@ public:
 
 	void Update(float frameTime);
 	void Render(Camera *camera);
+
+	void setGold(Gold* gold) { m_gold = gold; }
+	Gold* getGold() {return m_gold;}
+	Matrix getTranslationMatrix() { return m_translationMatrix; }
+	void createGoldObject();
 };
