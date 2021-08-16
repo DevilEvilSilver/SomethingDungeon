34743ba4c0d6a34de4273b38621a42bbb7e387a6
Tien Nguyen <tien.nd184311@sis.hust.edu.vn>
2021-08-16 09:45:34 +0800

    + add class Trap

---
+++ TrainingFramework/Trap.cpp
@@ -0,0 +1,23 @@
+#include"stdafx.h"
+#include"Trap.h"
+#include "Renderer.h"
+
+Trap::Trap() {}
+
+Trap::Trap(std::string prefabID, Vector2 roomID, Matrix translationMatrix)
+	: Object(prefabID, roomID, translationMatrix) {
+
+}
+
+Trap::~Trap()
+{
+
+}
+
+void Trap::Update(float frameTime) {
+
+}
+
+void Trap::Render(Camera* camera) {
+	Renderer::GetInstance()->DrawAnimated(this, camera);
+}
