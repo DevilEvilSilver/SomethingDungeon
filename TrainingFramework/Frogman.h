5e285adc1a047b9acbf9a34f493afbbcd9e40450
Tien Nguyen <tien.nd184311@sis.hust.edu.vn>
2021-08-27 23:23:00 +0800

    + add class for Frogman

---
+++ TrainingFramework/Frogman.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "Goblin.h"
+
+class Frogman :
+    public Goblin
+{
+public:
+    Frogman(std::string prefabID, Vector2 roomID, Matrix translationMatrix);
+
+    void UniqueUpdate(float frameTime);
+
+    void Shoot(Vector2 target);
+    void Melee(Vector2 target);
+
+}
+;
