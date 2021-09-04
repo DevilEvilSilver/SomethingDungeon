13e9ef2fd67ce98d43a2a721effb4f9bef1c8a97
DevilEvilSilver <longtgm1142@gmail.com>
2021-09-04 15:08:10 +0700

    -update minimap.fs

--- Resources/Shaders/Minimap.fs
+++ Resources/Shaders/Minimap.fs
@@ -5,10 +5,8 @@ varying vec2 v_uv;
 varying float v_length;
 void main()
 {
-	
-	vec4 defaults = vec4(0.1,0.1,0.1,1.0);
+	vec4 defaults = vec4(1.0,1.0,1.0,0.0);
 	float lerpValue  = clamp((v_length - 0.001)/0.09,0.0,1.0);
 	vec4 results = texture2D(u_texture, u_spriteScale * v_uv);
-	gl_FragColor = lerpValue*defaults + (1.0- lerpValue)*results;//vec4(1,0,0,1); 
-	//gl_FragColor = mix(results, defaults, lerpVal);
+	gl_FragColor = lerpValue*defaults + (1.0- lerpValue)*results;
 }
